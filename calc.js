54579f1b8fd284dc5a6a0cd65dcb606acd8548bc
km44192 <km44192@zut.edu.pl>
2019-03-28 11:50:30 +0100

    javascript

---
+++ calc.js
@@ -0,0 +1,18 @@
+﻿const double = x => 2*x;
+const init= (e,x) =>
+{
+    console.log("Container:",document.getElementById('container'));
+    console.log(typeof e);
+    console.log(double (x));
+}
+
+window.addEventListener("DOMContentLoaded",e=> init(e,5));
+var a;
+function klik()
+{
+
+ a = parseInt(document.getElementById('Display').value)+a;
+a=
+document.getElementById('Display').innerHTML=a;
+}
+
